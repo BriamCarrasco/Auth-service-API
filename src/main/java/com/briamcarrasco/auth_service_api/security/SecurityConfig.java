d40f9b0c06619ea322cfda625b315a403db95007
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-10-30 16:10:07 -0300

    Se configuran todos los edpoints como publicos para entorno de desarrollo

--- src/main/java/com/briamcarrasco/auth_service_api/security/SecurityConfig.java
+++ src/main/java/com/briamcarrasco/auth_service_api/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.briamcarrasco.auth_service_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Clase de configuración de seguridad para la aplicación.
@@ -22,4 +24,16 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() 
+            );
+        return http.build();
+    }
+
 }
